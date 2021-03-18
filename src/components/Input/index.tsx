829948ef02ec6be9dcb4a3f795357c755105b8c3
Lucas Macedo <lucasmdc57@gmail.com>
2021-03-18 12:22:48 -0300

    parsing Input to ts

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -1,4 +1,5 @@
-import {
+import React, {
+  InputHTMLAttributes,
   useEffect,
   useRef,
   useState,
@@ -9,8 +10,12 @@ import { useField } from '@unform/core';
 
 import { Container } from './styles';
 
-const Input = ({ name, ...rest }) => {
-  const inputRef = useRef(null);
+interface IInputProps extends InputHTMLAttributes<HTMLInputElement> {
+  name: string;
+}
+
+const Input: React.FC<IInputProps> = ({ name, ...rest }) => {
+  const inputRef = useRef<HTMLInputElement>(null);
 
   const [isFocused, setIsFocused] = useState(false);
   const [isFilled, setIsFilled] = useState(false);
